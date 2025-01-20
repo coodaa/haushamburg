9a1ac2d682f9637e472e4e4f251f9040425dac5a
Florian Schneider <schneider.f@me.com>
2025-01-20 23:28:17 +0100

    font confusion

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: true, 
+    host: true, // Erlaubt den Zugriff auf localhost von anderen Geräten im Netzwerk
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"), // Definiert den Alias "@" für den src-Ordner
     },
   },
 });
