c51335b3dde23788789d8ac54fd4e01f8273d54e
Florian Schneider <schneider.f@me.com>
2025-01-20 21:38:54 +0100

    rotate fish landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,17 +12,19 @@
         >
           {{ previousWord }}
         </span>
-        <!-- Neues Wort mit Fade-in -->
+        <!-- Neues Wort mit Fade-in und Nachwackeln -->
         <span
-          v-if="currentWordVisible"
-          class="bubble-text animate-in"
-        >
-          {{ currentWord }}
-        </span>
+  v-if="currentWordVisible"
+  class="bubble-text animate-in"
+>
+  {{ currentWord }}
+</span>
         <img
           src="@/assets/fish.svg"
           alt="Fisch Icon"
           class="fisch-icon"
+          :class="{ wobble: isWobbling }"
+          :style="{ transform: `rotate(${rotation}deg)` }"
         />
       </div>
     </div>
@@ -41,6 +43,8 @@
   </div>
 </template>
 
+
+
 <script>
 import '../styles/Home.css'
 
@@ -48,12 +52,15 @@ export default {
   name: 'Home',
   data() {
     return {
-      words: ["Moin!", "Ahoi!", "Goden Appetit!"], // Liste der Wörter
+      words: ["Moin!", "Ahoi!", "Hallo!"], // Liste der Wörter
       currentIndex: 0,
       currentWord: "Moin!",
       previousWord: "",
       currentWordVisible: true,
       previousWordVisible: false,
+      rotation: 0, // Startwinkel für das SVG
+      rotationDirection: 1, // Steuerung der Rotationsrichtung: 1 = vorwärts, -1 = rückwärts
+      isWobbling: false, // Steuert, ob das SVG nachwackelt
     };
   },
   mounted() {
@@ -69,12 +76,39 @@ export default {
         this.currentWord = this.words[this.currentIndex]; // Neues Wort setzen
         this.currentWordVisible = false; // Neues Wort unsichtbar machen
 
+        // Rotationswinkel anpassen
+        this.rotation += 15 * this.rotationDirection;
+
+        // Richtung wechseln, wenn die Grenze von ±30 Grad erreicht ist
+        if (this.rotation >= 30 || this.rotation <= -30) {
+          this.rotationDirection *= -1; // Richtung umkehren
+        }
+
+        // CSS-Variable für die aktuelle Rotation setzen
+        this.updateRotationVariable();
+
+        // Nachwackeln aktivieren, aber erst nach der Drehung
+        this.triggerWobble();
+
         // Fade-out starten, neues Wort sichtbar machen nach Fade-out
         setTimeout(() => {
           this.previousWordVisible = false; // Altes Wort ausblenden
           this.currentWordVisible = true; // Neues Wort einblenden
-        }, 500); // Wartezeit für Fade-out
+        }, 400); // Wartezeit für Fade-out
       }, 3000); // Wechselt alle 3 Sekunden
+    },
+    updateRotationVariable() {
+      // Setzt die aktuelle Rotation als CSS-Variable für das Wackeln
+      document.documentElement.style.setProperty('--rotation-start', `${this.rotation}deg`);
+    },
+    triggerWobble() {
+      // Aktiviert das Nachwackeln kurz nach der Drehung
+      setTimeout(() => {
+        this.isWobbling = true; // Wackeln starten
+        setTimeout(() => {
+          this.isWobbling = false; // Wackeln stoppen
+        }, 500); // Nachwackeln dauert 0.5 Sekunden
+      }, 500); // Nachwackeln startet 0.5 Sekunden nach der Drehung
     },
     handleFadeOutEnd() {
       this.previousWordVisible = false; // Altes Wort komplett ausblenden
